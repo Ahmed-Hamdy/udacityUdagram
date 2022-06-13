42795d74181330e36987e8b6924ba1a6f5603292
Ahmed Hamdi ( ICT Networks - Dept. Head ) <Ahmed.Hamdi@KHALDA-EG.com>
2022-06-13 10:43:53 +0200

    Hosting a Full-Stack Application

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   appName: "Udagram",
   //apiHost: "http://localhost:8080/api/v0",
-  apiHost: "http://udagram-api-dev.eba-jwtb3tmw.us-east-1.elasticbeanstalk.com/:8080/api/v0",
+  apiHost: "http://udagram-api-dev.eba-jwtb3tmw.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
